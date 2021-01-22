814c2f53e480fc089abfdabf214aad6759f891fb
fenghao-hgg <2437924014@qq.com>
2021-01-22 19:39:38 +0800

    第一次上传

---
+++ src/main/java/doc/Demo.java
@@ -0,0 +1,25 @@
+package doc;
+/**
+ * 文档注释只用在三个地方：类上，方法上和常量上
+ * 文档注释是功能及注释，在类上使用时是用来说明当前类的设计意图和整体功能
+ *
+ * @author fenghao
+ */
+public class Demo {
+    /**
+     * sayHello中的问候语
+     */
+    public static final String INFO="你好！";
+
+    /**
+     * 为指定的用户添加一个问候语
+     * @param name  指定的用户的名字
+     * @return     含有问候语的字符串
+     */
+    public String sayHello(String name){
+        return "你好！"+name;
+
+    }
+
+
+}
---
+++ src/main/java/string/ValueOfDemo.java
@@ -0,0 +1,23 @@
+package string;
+
+import javax.imageio.stream.ImageInputStream;
+
+/**
+ *static String valueOf(XXX xxx)
+ * String 提供了一组重载的静态方法：valueOf。作用是将java中其他类型转换为String
+ * 最长使用于将基本类型转换为字符串
+ */
+public class ValueOfDemo {
+    public static void main(String[] args) {
+        int i = 123;
+        String str = String.valueOf(i);
+        System.out.println(str);
+
+        double dou = 123.123;
+        String str2 = String.valueOf(dou);
+        System.out.println(str2);
+
+        String s3 = i + ""; //任何内容与字符串连接结果都是字符串
+        System.out.println(s3);
+    }
+}
